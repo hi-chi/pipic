d0dc94146d0974b828424b6668a62c223d97b333
Arkady Gonoskov <33511833+arkady-gonoskov@users.noreply.github.com>
2023-11-10 13:27:04 +0100

    Update __init__.py
    license statement corrected

--- pipic/__init__.py
+++ pipic/__init__.py
@@ -6,7 +6,7 @@ Main module of the pipic package.
 __project__ = 'pipic'
 __description__ = 'A Python library for particle-in-cell plasma simulation.'
 __copyright__ = '2023'
-__license__ = 'GNU General Public License, version 3 (GPLv3)'
+__license__ = 'GNU General Public License v3 (GPLv3)'
 __version__ = '1.0'
 __maintainer__ = 'The pipic developers team'
 __maintainer_email__ = 'arkady.gonoskov@physics.gu.se'
