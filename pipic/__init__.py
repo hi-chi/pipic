d44b2252055e91d600abd8e6c530968b14298aab
Arkady Gonoskov <33511833+arkady-gonoskov@users.noreply.github.com>
2023-11-10 14:37:04 +0100

    Update __init__.py
    minor changes: author, email

--- pipic/__init__.py
+++ pipic/__init__.py
@@ -5,10 +5,11 @@ Main module of the pipic package.
 
 __project__ = 'pipic'
 __description__ = 'A Python library for particle-in-cell plasma simulation.'
+__author__ = ['Arkady Gonoskov']
+__author_email__ = 'arkady.gonoskov@physics.gu.se'
 __copyright__ = '2023'
 __license__ = 'GNU General Public License v3 (GPLv3)'
 __version__ = '1.0'
 __maintainer__ = 'The pipic developers team'
-__maintainer_email__ = 'arkady.gonoskov@physics.gu.se'
 __status__ = 'Stable'
 # __url__ = ''
