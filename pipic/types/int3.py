20015c5b1b2bcf027423b1b51f8a12bec4c33776
Joel Magnusson <11462718+joel-magnusson@users.noreply.github.com>
2023-12-07 10:43:46 +0100

    Add missing license statements

--- pipic/types/int3.py
+++ pipic/types/int3.py
@@ -1,3 +1,24 @@
+"""
+-------------------------------------------------------------------------------------------------------
+This file is part of pi-PIC.
+pi-PIC, Copyright 2023 Arkady Gonoskov
+---------------------------------------------------------------------------------------------------------
+pi-PIC is free software: you can redistribute it and/or modify it under the terms of the GNU General
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+pi-PIC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with pi-PIC. If not, se
+<https://www.gnu.org/licenses/>.
+---------------------------------------------------------------------------------------------------------
+Website: https://github.com/hi-chi/pipic
+Contact: arkady.gonoskov@gu.se.
+-------------------------------------------------------------------------------------------------------*/
+"""
+
 import numba as nb
 from numba.experimental import jitclass
 
