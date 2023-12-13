a1e7df2015e0ec400f6319998a99d9db0a7ddcc8
Joel Magnusson <11462718+joel-magnusson@users.noreply.github.com>
2023-12-13 14:20:14 +0100

    Add compton and breit-wheeler rates

--- src/extensions/qed/qed.cpp
+++ src/extensions/qed/qed.cpp
@@ -23,136 +23,23 @@ Contact: arkady.gonoskov@gu.se.
 #include "pybind11/stl.h"
 #include <pybind11/operators.h>
 
+#include "compton.h"
+#include "breit_wheeler.h"
+
 const string name = "qed";
 static int photonTypeId, electronTypeId, positronTypeId; // particle types
+static pfc::Compton compton;
+static pfc::Breit_wheeler breit_wheeler;
 
-void onePhotonStep(particle &P, const double3 &E, const double3 &B, double &time, double timeStep, int type, vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles) {
-    double SchwingerField = sqr(constants::electronMass * constants::lightVelocity) * constants::lightVelocity
-                            / (-constants::electronCharge * constants::hbar);
-    double c = constants::lightVelocity;
-    double mc = constants::electronMass * constants::lightVelocity; // Change to real particle mass
-
-    while (time < timeStep) {
-        double p = P.p.norm();
-        double gamma = p/mc; // gamma = E/mc2 = |p|/mc
-
-        double3 k_ = P.p/p; // normalized wave vector
-
-        double H_eff = sqrt(sqr(E + cross(k_, B)) - sqr(dot(E, k_)));
-        double chi = gamma * H_eff / SchwingerField;
-        double rate = 0.0, dt = 2*timeStep;
-
-        // Compute rate and subtimestep (dt)
-        if (chi > 0.0) {
-            // rate = breit_wheeler.rate(chi);
-            // dt = getDtPhoton(particle, rate, chi, gamma);
-        }
-
-        if (dt + time > timeStep) {
-            // Move particle to end of timeStep. No particle push being carried out in this part of code
-            time = timeStep;
-        } else {
-            // Move particle by a subtimestep (dt). No particle push being carried out in this part of code
-            time += dt;
-
-            // determine new particle energy
-            double delta = 0.5; // Pair_Generator(chi);
-
-            // Create new particle (electron)
-            particle new_particle;
-            new_particle.w = P.w;
-            new_particle.r = P.r;
-            new_particle.p = delta * P.p;
-
-            // Add new particle to container for later processing
-            AvalancheParticles.push_back(new_particle);
-            timeAvalancheParticles.push_back(time);
-            typeAvalancheParticles.push_back(electronTypeId);
-
-            // Create new particle (positron)
-            new_particle.p = (1-delta) * P.p;
-
-            // Add new particle to container for later processing
-            AvalancheParticles.push_back(new_particle);
-            timeAvalancheParticles.push_back(time);
-            typeAvalancheParticles.push_back(positronTypeId);
-
-            // Change current particle momentum
-            P.p = 0.0 * P.p;
-            P.w = 0.0; // Marks particle for deletion
-        }
-    }
-}
-
-void oneParticleStep(particle &P, const double3 &E, const double3 &B, double &time, double timeStep, int type, vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles) {
-    double SchwingerField = sqr(constants::electronMass * constants::lightVelocity) * constants::lightVelocity
-                            / (-constants::electronCharge * constants::hbar);
-    double c = constants::lightVelocity;
-    double mc = constants::electronMass * constants::lightVelocity; // Change to real particle mass
-
-    while (time < timeStep) {
-        double gamma = sqrt(1 + sqr(P.p)/sqr(mc));
-        double3 beta = P.p / (gamma*mc);
-
-        double H_eff = sqr(E + cross(beta, B)) - sqr(dot(E, beta));
-        if (H_eff < 0) H_eff = 0;
-        H_eff = sqrt(H_eff);
-
-        double chi = gamma * H_eff / SchwingerField;
-        double rate = 0.0, dt = 2*timeStep;
-
-        // Compute rate and subtimestep (dt)
-        if (chi > 0.0) {
-            // rate = compton.rate(chi);
-            // dt = getDtParticle(particle, rate, chi);
-        }
-
-        if (dt + time > timeStep) {
-            // Move particle to end of timeStep. No particle push being carried out in this part of code
-            time = timeStep;
-        } else {
-            // Move particle by a subtimestep (dt). No particle push being carried out in this part of code
-            time += dt;
-
-            // determine new particle energy
-            double delta = 0.5; // Photon_Generator((chi + chi_new)/(FP)2.0);
-
-            // Create new particle (photon)
-            particle new_particle;
-            new_particle.w = P.w;
-            new_particle.r = P.r;
-            new_particle.p = delta * P.p;
-
-            // Add new particle to container for later processing
-            AvalancheParticles.push_back(new_particle);
-            timeAvalancheParticles.push_back(time);
-            typeAvalancheParticles.push_back(photonTypeId);
-
-            // Change current particle momentum
-            P.p = (1 - delta) * P.p;
-        }
-    }
-}
-
-
-void RunAvalanche(const double3 E, const double3 B, double timeStep, vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles) {
-    // Track the number of processed particles
-    int countParticles = 0;
-
-    while (countParticles != AvalancheParticles.size() || false) {
-        for (int k = countParticles; k!=AvalancheParticles.size(); k++) {
-            int typeId = typeAvalancheParticles[k];
-            // Process a single particle a full timeStep
-            if (typeId == electronTypeId || typeId == positronTypeId) {
-                oneParticleStep(AvalancheParticles[k], E, B, timeAvalancheParticles[k], timeStep, typeId, AvalancheParticles, timeAvalancheParticles, typeAvalancheParticles);
-                countParticles++;
-            } else if (typeId == photonTypeId) {
-                onePhotonStep(AvalancheParticles[k], E, B, timeAvalancheParticles[k], timeStep, typeId, AvalancheParticles, timeAvalancheParticles, typeAvalancheParticles);
-                countParticles++;
-            }
-        }
-    }
-}
+void RunAvalanche(const double3 E, const double3 B, double timeStep,
+                    vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles);
+void oneParticleStep(particle &P, const double3 &E, const double3 &B, double &time, double timeStep, int type,
+                    vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles);
+void onePhotonStep(particle &P, const double3 &E, const double3 &B, double &time, double timeStep, int type,
+                    vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles);
+// double getDt(double rate, double chi, double gamma);
+// double Photon_Generator(double chi);
+// double Pair_Generator(double chi);
 
 // function that is called to process particles in a given cell
 void Handler(int *I, double *D, double *F, double *P, double *NP, double *dataDouble, int *dataInt) {
@@ -194,8 +81,8 @@ void Handler(int *I, double *D, double *F, double *P, double *NP, double *dataDo
             P->w = AvalancheParticles[0].w;
 
             // Place all created particles in the buffer
-            for (int k = 1; k < AvalancheParticles.size(); k++) {
-                // Make sure that particle isn't marked for deletion
+            for (int k = 1; k < int(AvalancheParticles.size()); k++) {
+                // Make sure that the particle isn't marked for deletion
                 if (AvalancheParticles[k].w > 0.0) {
                     // Check if the buffer permits adding a particle
                     if(CI.particleBufferSize < CI.particleBufferCapacity){
@@ -210,6 +97,157 @@ void Handler(int *I, double *D, double *F, double *P, double *NP, double *dataDo
         }
     }
 };
+
+
+void RunAvalanche(const double3 E, const double3 B, double timeStep, vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles) {
+    // Track the number of processed particles
+    int countParticles = 0;
+
+    while (countParticles != int(AvalancheParticles.size()) || false) {
+        for (int k = countParticles; k != int(AvalancheParticles.size()); k++) {
+            int typeId = typeAvalancheParticles[k];
+            // Process a single particle a full timeStep
+            if (typeId == electronTypeId || typeId == positronTypeId) {
+                oneParticleStep(AvalancheParticles[k], E, B, timeAvalancheParticles[k], timeStep, typeId, AvalancheParticles, timeAvalancheParticles, typeAvalancheParticles);
+                countParticles++;
+            } else if (typeId == photonTypeId) {
+                onePhotonStep(AvalancheParticles[k], E, B, timeAvalancheParticles[k], timeStep, typeId, AvalancheParticles, timeAvalancheParticles, typeAvalancheParticles);
+                countParticles++;
+            }
+        }
+    }
+}
+
+
+void oneParticleStep(particle &P, const double3 &E, const double3 &B, double &time, double timeStep, int type, vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles) {
+    double SchwingerField = sqr(constants::electronMass * constants::lightVelocity) * constants::lightVelocity
+                            / (-constants::electronCharge * constants::hbar);
+    // double c = constants::lightVelocity;
+    double mc = constants::electronMass * constants::lightVelocity; // Change to real particle mass
+
+    while (time < timeStep) {
+        double gamma = sqrt(1 + sqr(P.p)/sqr(mc));
+        double3 beta = P.p / (gamma*mc);
+
+        double H_eff = sqr(E + cross(beta, B)) - sqr(dot(E, beta));
+        if (H_eff < 0) H_eff = 0;
+        H_eff = sqrt(H_eff);
+
+        double chi = gamma * H_eff / SchwingerField;
+        double rate = 0.0, dt = 2*timeStep;
+
+        // Compute rate and subtimestep (dt)
+        if (chi > 0.0) {
+            rate = compton.rate(chi);
+            // dt = getDt(rate, chi, gamma);
+        }
+
+        if (dt + time > timeStep) {
+            // Move particle to end of timeStep. No particle push being carried out in this part of code
+            time = timeStep;
+        } else {
+            // Move particle by a subtimestep (dt). No particle push being carried out in this part of code
+            time += dt;
+
+            // determine new particle energy
+            double delta = 0.5; // Photon_Generator(chi);
+
+            // Create new particle (photon)
+            particle new_particle;
+            new_particle.w = P.w;
+            new_particle.r = P.r;
+            new_particle.p = delta * P.p;
+
+            // Add new particle to container for later processing
+            AvalancheParticles.push_back(new_particle);
+            timeAvalancheParticles.push_back(time);
+            typeAvalancheParticles.push_back(photonTypeId);
+
+            // Change current particle momentum
+            P.p = (1 - delta) * P.p;
+        }
+    }
+}
+
+
+void onePhotonStep(particle &P, const double3 &E, const double3 &B, double &time, double timeStep, int type, vector<particle> &AvalancheParticles, vector<double> &timeAvalancheParticles, vector<int> &typeAvalancheParticles) {
+    double SchwingerField = sqr(constants::electronMass * constants::lightVelocity) * constants::lightVelocity
+                            / (-constants::electronCharge * constants::hbar);
+    // double c = constants::lightVelocity;
+    double mc = constants::electronMass * constants::lightVelocity; // Change to real particle mass
+
+    while (time < timeStep) {
+        double p = P.p.norm();
+        double gamma = p/mc; // gamma = E/mc2 = |p|/mc
+
+        double3 k_ = P.p/p; // normalized wave vector
+
+        double H_eff = sqrt(sqr(E + cross(k_, B)) - sqr(dot(E, k_)));
+        double chi = gamma * H_eff / SchwingerField;
+        double rate = 0.0, dt = 2*timeStep;
+
+        // Compute rate and subtimestep (dt)
+        if (chi > 0.0) {
+            rate = breit_wheeler.rate(chi);
+            // dt = getDt(rate, chi, gamma);
+        }
+
+        if (dt + time > timeStep) {
+            // Move particle to end of timeStep. No particle push being carried out in this part of code
+            time = timeStep;
+        } else {
+            // Move particle by a subtimestep (dt). No particle push being carried out in this part of code
+            time += dt;
+
+            // determine new particle energy
+            double delta = 0.5; // Pair_Generator(chi);
+
+            // Create new particle (electron)
+            particle new_particle;
+            new_particle.w = P.w;
+            new_particle.r = P.r;
+            new_particle.p = delta * P.p;
+
+            // Add new particle to container for later processing
+            AvalancheParticles.push_back(new_particle);
+            timeAvalancheParticles.push_back(time);
+            typeAvalancheParticles.push_back(electronTypeId);
+
+            // Create new particle (positron)
+            new_particle.p = (1-delta) * P.p;
+
+            // Add new particle to container for later processing
+            AvalancheParticles.push_back(new_particle);
+            timeAvalancheParticles.push_back(time);
+            typeAvalancheParticles.push_back(positronTypeId);
+
+            // Change current particle momentum
+            P.p = 0.0 * P.p;
+            P.w = 0.0; // Marks particle for deletion
+        }
+    }
+}
+
+
+// double getDt(double rate, double chi, double gamma) {
+//     double r = -log(random_number_omp());
+//     r *= (gamma) / (chi);
+//     return r / rate;
+// }
+
+// double Photon_Generator(double chi) {
+//     double r = random_number_omp();
+//     return compton.inv_cdf(r, chi);
+// }
+
+// double Pair_Generator(double chi) {
+//     double r = random_number_omp();
+//     if (r < 0.5)
+//         return breit_wheeler.inv_cdf(r, chi);
+//     else
+//         return 1.0 - breit_wheeler.inv_cdf(1.0 - r, chi);
+// }
+
 
 // extension initialization
 int64_t handler(int photonType, int electronType, int positronType){
