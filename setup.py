f4ae992debfa7f8e8ff0922b866fff4a45b16d89
Joel Magnusson <11462718+joel-magnusson@users.noreply.github.com>
2023-12-19 14:44:36 +0100

    Revert back to not run CI on macos

--- setup.py
+++ setup.py
@@ -79,11 +79,11 @@ class BuildExt(build_ext):
         opts = self.c_opts.get(ct, [])
         if sys.platform == 'darwin':
             if has_flag(self.compiler, '-stdlib=libc++'):
-                opts.append('-stdlib=libc++')
-            if has_flag(self.compiler, '-Xclang -fopenmp'):
-                opts.append('-Xclang -fopenmp')
-            if has_flag(self.compiler, '-lomp'):
-                opts.append('-lomp')
+                pass  # opts.append('-stdlib=libc++')
+            # if has_flag(self.compiler, '-Xclang -fopenmp'):
+            #     opts.append('-Xclang -fopenmp')
+            # if has_flag(self.compiler, '-lomp'):
+            #     opts.append('-lomp')
         if ct == 'unix':
             opts.append("-DVERSION_INFO='{}'"
                         .format(self.distribution.get_version()))
@@ -95,7 +95,7 @@ class BuildExt(build_ext):
                         .format(self.distribution.get_version()))
         opts.append('-O3')
         opts.append('-fPIC')
-        # opts.append('-fopenmp')
+        opts.append('-fopenmp')
         opts.append('-lfftw3')
         for ext in self.extensions:
             ext.extra_compile_args = opts
