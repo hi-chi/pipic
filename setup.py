a7e0c3ff172911eb5f3d7d978dc576b5ef4ee78a
Joel Magnusson <11462718+joel-magnusson@users.noreply.github.com>
2024-05-01 12:33:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,12 +41,13 @@ def has_flag(compiler, flagname):
     import tempfile
     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
         f.write('int main (int argc, char **argv) { return 0; }')
-        print("\033[33mNOTE\033[0m: The following is just a compiler flag test.")
+        print("\033[33mNOTE: The following is just a compiler flag test.\033[0m")
         try:
             compiler.compile([f.name], extra_postargs=[flagname])
         except setuptools.distutils.errors.CompileError:
             print('\033[33mCompiler flag test failed! Flag will be ignored.\033[0m')
             return False
+    print("\033[33mCompiler flag test completed.\033[0m")
     return True
 
 
