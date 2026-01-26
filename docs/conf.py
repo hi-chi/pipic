e0d7b044a21c01f64a56bdd4ede7b5cfa920537c
Frida <Fraidowolf>
2026-01-26 17:22:42 +0100

    adding .rts files for read-the-docs

---
+++ docs/conf.py
@@ -0,0 +1,88 @@
+# Configuration file for Sphinx documentation builder.
+# For full options, see the Sphinx documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+# Add parent directory to path for autodoc
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information --
+project = 'π-PIC'
+copyright = '2026, LWFA Team'
+author = 'LWFA Team'
+
+# Full version
+release = '1.0'
+version = '1.0.0'
+
+# -- General configuration --
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+]
+
+# Source file suffix
+source_suffix = '.rts'
+
+# Master doc (in docs/ root)
+master_doc = 'index'
+
+# List of patterns to exclude
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Language
+language = 'en'
+
+# -- Options for HTML output --
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_logo = 'logo/pipic_logo.png' if os.path.exists('logo/pipic_logo.png') else None
+html_theme_options = {
+    'fixed_sidebar': True,
+    'sidebar_width': '250px',
+    'page_width': '960px',
+}
+
+# -- Options for LaTeX output --
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '11pt',
+    'preamble': r'''
+\usepackage{amsmath}
+\usepackage{amssymb}
+''',
+}
+
+latex_documents = [
+    ('index', 'pipic.tex', 'π-PIC Documentation',
+     'LWFA Team', 'manual'),
+]
+
+# -- Options for reStructuredText --
+rst_prolog = r"""
+.. |pi-pic| replace:: π-PIC
+.. |cgs| replace:: CGS
+"""
+
+# -- Math configuration (MathJax 3) --
+mathjax3_config = {
+    'tex': {
+        'inlinemath': [('$', '$')],
+        'displaymath': [('$$', '$$')],
+    },
+}
+
+# -- Autodoc configuration --
+autodoc_typehints = 'description'
+autodoc_member_order = 'bysource'
+
+# -- Intersphinx mapping --
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+}
